b07d73503b974e1a63ed41008b9f85268cd342ae
miguel-kjh <miguelangel.kjh@gmail.com>
2024-11-25 13:21:46 +0000

    add parse

--- data_processor.py
+++ data_processor.py
@@ -8,12 +8,13 @@ from data_preprare.generate_data_stats import StatsGenerator
 from tqdm import tqdm
 
 def download():
-    for lang in LANGUAGES:
-        download_dataset(lang, RAW_DATA_FOLDER)
+    #for lang in LANGUAGES:
+    #    download_dataset(lang, RAW_DATA_FOLDER)
     print("All datasets downloaded and saved")
 
 def stats():
-    stats_gen = StatsGenerator()
+    print("Generating stats")
+    """stats_gen = StatsGenerator()
     for lang in tqdm(LANGUAGES, desc="Processing languages"):
         dataset_name = os.path.join(RAW_DATA_FOLDER, lang)
         assert os.path.exists(dataset_name), f"Dataset {dataset_name} does not exist"
@@ -21,13 +22,21 @@ def stats():
         stats_gen.add_lang(dataset, lang)
 
     df = stats_gen.get_stats()
-    df.to_csv(FILE_STATS, index=False)
+    df.to_csv(FILE_STATS, index=False)"""
 
 def process():
+    print("Processing data")
     pass
 
-def parse():
-    pass
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument(
+        "--operation",
+        type=str,
+        choices=OPERATIONS.keys(),
+        help="Operation to perform",
+    )
+    return parse.parse_args()
 
 OPERATIONS = {
     "download": download,
@@ -37,5 +46,7 @@ OPERATIONS = {
 
 
 if __name__ == '__main__':
-    #download()
-    stats()
+    args = parse_args()
+    operation = args.operation
+    assert operation in OPERATIONS, f"Operation {operation} not found"
+    OPERATIONS[operation]()
