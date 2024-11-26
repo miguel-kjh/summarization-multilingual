1368286f063a09e41ab8a33a2403139c6bc6c532
miguel-kjh <miguelangel.kjh@gmail.com>
2024-11-26 14:42:54 +0000

    add litgpt

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from litgpt import LLM
 
-llm = LLM.load("EleutherAI/pythia-14m")
-text = llm.generate("Fix the spelling: Every fall, the familly goes to the mountains.")
+llm_l1 = LLM.load("EleutherAI/pythia-160m")
+print(llm_l1.model.transformer.wte)
+
+llm_l2 = LLM.load("EleutherAI/pythia-14m")
+print(llm_l2.model.transformer.wte)
+
+text = llm_l1.generate("Fix the spelling: Every fall, the familly goes to the mountains.")
 print(text)
 # Corrected Sentence: Every fall, the family goes to the mountains.       
