881f833727ee91f1ca77d0f365a7db6a7af06e7a
miguel-kjh <miguelangel.kjh@gmail.com>
2024-12-11 08:42:53 +0000

    change embedding projections

--- embedding_distilation/EmbeddingAlignmentModel.py
+++ embedding_distilation/EmbeddingAlignmentModel.py
@@ -8,7 +8,7 @@ from embedding_distilation.ProjectionHead import ProjectionHead
 class EmbeddingAlignmentModel(pl.LightningModule):
     def __init__(self, large_embedding_dim, small_embedding_dim, projected_dim, inner_dim, lr=1e-3):
         super().__init__()
-        self.projector = ProjectionHead(large_embedding_dim, projected_dim)
+        self.projector = ProjectionHead(projected_dim, large_embedding_dim)
         self.connector = Connector(small_embedding_dim, projected_dim, inner_dim)
         self.lr = lr
         self.criterion = torch.nn.MSELoss() # L2 norm error
@@ -22,28 +22,28 @@ class EmbeddingAlignmentModel(pl.LightningModule):
         # embedding_large = embedding_large / (torch.norm(embedding_large, dim=-1, keepdim=True) + 1e-8)
         # embedding_small = embedding_small / (torch.norm(embedding_small, dim=-1, keepdim=True) + 1e-8)
 
-        projected_large = self.projector(embedding_large)
         transformed_small = self.connector(embedding_small)
-        return projected_large, transformed_small
+        projected_large = self.projector(transformed_small)
+        return projected_large
 
     def training_step(self, batch, batch_idx):
         embedding_large, embedding_small = batch
-        projected_large, transformed_small = self(embedding_large, embedding_small)
-        loss = self.criterion(projected_large, transformed_small)
+        projected_large = self(embedding_large, embedding_small)
+        loss = self.criterion(projected_large, embedding_large)
 
         # Logging adicional
-        similarity = torch.nn.functional.cosine_similarity(projected_large, transformed_small, dim=-1).mean()
+        similarity = torch.nn.functional.cosine_similarity(projected_large, embedding_large, dim=-1).mean()
         self.log("train_loss", loss, prog_bar=True)
         self.log("cosine_similarity", similarity, prog_bar=True)
         return loss
     
     def validation_step(self, batch, batch_idx):
         embedding_large, embedding_small = batch
-        projected_large, transformed_small = self(embedding_large, embedding_small)
-        loss = self.criterion(projected_large, transformed_small)
+        projected_large = self(embedding_large, embedding_small)
+        loss = self.criterion(projected_large, embedding_large)
 
         # Logging adicional
-        similarity = torch.nn.functional.cosine_similarity(projected_large, transformed_small, dim=-1).mean()
+        similarity = torch.nn.functional.cosine_similarity(projected_large, embedding_large, dim=-1).mean()
         self.log("val_loss", loss, prog_bar=True)
         self.log("cosine_similarity_val", similarity, prog_bar=True)
         return loss
