a1e0535803bb421be05011cd987b4bf3012d568b
miguel-kjh <miguelangel.kjh@gmail.com>
2025-01-02 18:54:31 +0000

    update

--- clustering_split.py
+++ clustering_split.py
@@ -23,7 +23,7 @@ embedding_model_path = 'sentence-transformers/paraphrase-multilingual-mpnet-base
 model_spacy = 'es_core_news_sm'
 distance_metric = 'cosine'
 name_new_dataset = "data/02-processed/spanish/train_cluster.pkl"
-number_samples = 0.01
+number_samples = None
 top_k_sents = None
 
 def load_dataset_and_model(dataset_path: str, embedding_model_path: str) -> tuple:
