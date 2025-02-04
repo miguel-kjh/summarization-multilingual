89a89846835a4e1edc34c5f6f36d000e3d52fca2
miguel-kjh <miguelangel.kjh@gmail.com>
2025-02-04 13:48:43 +0000

    upadte

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-model="models/Qwen2.5-0.5B-Instruct-spanish-chunks-openai-e2-b1-lr0.0001-wd0.0-c1024-peft-lora-r8-a16-d0.05-2025-01-27-22-34-11"
+model="models/Qwen2.5-0.5B-Instruct-spanish-paragraphs-sentence-transformers-e2-b1-lr0.0001-wd0.0-c1024-r8-a16-d0.05-quant-2025-01-25-15-31-30"
 wandb=False
 method="clustering"
 
--- generate.py
+++ generate.py
@@ -15,8 +15,8 @@ from utils import create_model_and_tokenizer, seed_everything, SEED
 def parse():
     parser = argparse.ArgumentParser(description="Script to generate summaries")
 
-    parser.add_argument("--model_name_or_path", type=str, default="models/Qwen2.5-0.5B-Instruct-spanish-chunks-openai-e2-b1-lr0.0001-wd0.0-c1024-peft-lora-r8-a16-d0.05-2025-01-27-22-34-11", help="Model name")
-    parser.add_argument("--dataset", type=str, default="data/04-clustering/spanish-chunks-openai", help="Dataset path")
+    parser.add_argument("--model_name_or_path", type=str, default="models/Qwen2.5-0.5B-Instruct-spanish-paragraphs-sentence-transformers-e2-b1-lr0.0001-wd0.0-c1024-r8-a16-d0.05-quant-2025-01-25-15-31-30", help="Model name")
+    parser.add_argument("--dataset", type=str, default="data/04-clustering/spanish-paragraphs-sentence-transformers", help="Dataset path")
     parser.add_argument("--data_sample", type=int, default=10, help="Size of the data sample")
     parser.add_argument("--max_new_tokens", type=int, default=512, help="Maximum number of new tokens")
     parser.add_argument("--using_clustering", type=lambda x: bool(strtobool(x)), default=True, help="Clustering method to use")
